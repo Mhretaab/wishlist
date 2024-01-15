9484d5754e8280ace1d52317df8b7a3ae7257044
Mhretaab Berhe <mhretberhe@gmail.com>
2024-01-15 22:34:24 +0100

    added Template-driven Form, different ui controls data and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   wishes: WishItem[] = [
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
 
+  newWishText = '';
+  listFilter: string = '0';
+
   title = 'wishlist';
+
+  visibleWishes: WishItem[] = this.wishes;
+
+
+  addNewWish(): void {
+    this.wishes.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  filterChanged(event: any): void {
+    if (event === '0') {
+      this.visibleWishes = this.wishes;
+    } else if (event === '1') {
+      this.visibleWishes = this.wishes.filter(item => !item.isCompleted);
+    } else {
+      this.visibleWishes = this.wishes.filter(item => item.isCompleted);
+    }
+  }
 
   toggleItem(wishItem: WishItem): void {
     wishItem.isCompleted = !wishItem.isCompleted;
     console.log(wishItem);
   }
+
 }
